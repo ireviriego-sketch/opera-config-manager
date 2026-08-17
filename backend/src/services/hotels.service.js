65a884eff6fa599b9c7ddc82c0cb42bb7ad8c0a3
Nacho Reviriego <i.reviriego@accenture.com>
2026-08-17 12:52:39 +0200

    remove backend dead code

--- backend/src/services/hotels.service.js
+++
@@ -1,7 +0,0 @@
-const hotelsRepository = require('../repositories/hotels.repository');
-
-async function listHotels(filters = {}) {
-  return hotelsRepository.findAll(filters);
-}
-
-module.exports = { listHotels };
